e84db49cd44873540af883ce8cdf903049144524
xavperrin <xl.perrin@gmail.com>
2023-01-10 22:10:18 +0100

    hex

--- hex.spec.js
+++ hex.spec.js
@@ -1,10 +1,17 @@
 describe("hex.js", ()=>{
 
     describe("getRandomhexanumber()", ()=>{
-        
+        it('should return a number', function(){
+            
+            // expect return a number;
+            expect(getRandomhexanumber()).toMatch(/-?\d+/);
+            expect(typeof getRandomhexanumber()).toMatch('number');
+            
+        });
         it('should return a number between [0,15]', function(){
             
-            // expect return a number between [0 & 3];
+            // expect return a number between [0 & 15];
+
             expect(getRandomhexanumber()<=15).toBeTruthy();
             expect(getRandomhexanumber()>=0).toBeTruthy();
             expect(getRandomhexanumber()).toBeGreaterThanOrEqual(0);
@@ -18,6 +25,10 @@ describe("hex.js", ()=>{
             
             // expect return a string (e.g. "#f18901") ;
             expect(getRandomHexadecimalColor().length===7).toBeTruthy();
+
         });
+        it('should return an hexadecimal color (e.g. "#F18901")'), ()=>{
+            expect(getRandomHexadecimalColor()).toMatch(/#[0-9a-fA-F]{6}/);
+        }
     });
 })
